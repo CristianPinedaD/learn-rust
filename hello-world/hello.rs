6c7bc13e94723a5aeaf7a5991d8672bf2d7dea7c
CristianPinedaD <109752965+CristianPinedaD@users.noreply.github.com>
2025-03-12 15:02:35 -0400

    done

---
+++ hello-world/hello.rs
@@ -0,0 +1,16 @@
+// This is a comment, and is ignored by the compiler
+
+// A binary can be generated using the rust compiler: rustc
+// $ rustc hello.rs
+// A binary will be produced that can be executed with ./hello
+
+// This is the main function:
+
+fn main() {
+    // statements here are executed when the compiled binary is called.
+
+    // Print text to the console.
+
+    println!("Hello world!");  // println! is a macro printing text to the console.
+    println!("I'm a Rustacean!");
+}
